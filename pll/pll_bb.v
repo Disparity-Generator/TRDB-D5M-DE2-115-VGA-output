040d1551055bda7379f362cb7d7566ef3eae757e
TimSchulz <11016854+Sch-Tim@users.noreply.github.com>
2024-02-05 16:15:03 +0100

    Add files via upload

---
+++ pll/pll_bb.v
@@ -0,0 +1,24 @@
+
+module pll (
+	clk_clk,
+	clk_camera_clk,
+	clk_double_system_clk,
+	clk_i2c_clk,
+	clk_i2c_double_clk,
+	clk_pll_camera_clk,
+	clk_sdram_clk,
+	clk_system_clk,
+	clk_vga_clk,
+	reset_reset_n);	
+
+	input		clk_clk;
+	output		clk_camera_clk;
+	output		clk_double_system_clk;
+	output		clk_i2c_clk;
+	output		clk_i2c_double_clk;
+	output		clk_pll_camera_clk;
+	output		clk_sdram_clk;
+	output		clk_system_clk;
+	output		clk_vga_clk;
+	input		reset_reset_n;
+endmodule
---
+++ pll/pll_inst.v
@@ -0,0 +1,13 @@
+	pll u0 (
+		.clk_clk               (<connected-to-clk_clk>),               //               clk.clk
+		.clk_camera_clk        (<connected-to-clk_camera_clk>),        //        clk_camera.clk
+		.clk_double_system_clk (<connected-to-clk_double_system_clk>), // clk_double_system.clk
+		.clk_i2c_clk           (<connected-to-clk_i2c_clk>),           //           clk_i2c.clk
+		.clk_i2c_double_clk    (<connected-to-clk_i2c_double_clk>),    //    clk_i2c_double.clk
+		.clk_pll_camera_clk    (<connected-to-clk_pll_camera_clk>),    //    clk_pll_camera.clk
+		.clk_sdram_clk         (<connected-to-clk_sdram_clk>),         //         clk_sdram.clk
+		.clk_system_clk        (<connected-to-clk_system_clk>),        //        clk_system.clk
+		.clk_vga_clk           (<connected-to-clk_vga_clk>),           //           clk_vga.clk
+		.reset_reset_n         (<connected-to-reset_reset_n>)          //             reset.reset_n
+	);
+
